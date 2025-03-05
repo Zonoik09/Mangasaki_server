c1ed548403230389e909261b0b53e60fe6c947c5
pablo <pablovicenteroura2005@gmail.com>
2025-03-05 20:55:05 +0100

    commit

--- server/src/routes/socialRoutes.js
+++ server/src/routes/socialRoutes.js
@@ -1,21 +1,3 @@
 const express = require('express');
 const router = express.Router();
-const {
-    listOllamaModels,
-} = require('../controllers/socialController');
-
-/**
- * @swagger
- * /api/chat/models:
- *   get:
- *     summary: Lista los modelos disponibles en Ollama
- *     tags: [Social]
- *     responses:
- *       200:
- *         description: Lista de modelos disponibles
- *       500:
- *         description: Error al recuperar modelos
- */
-router.get('/models', listOllamaModels);
-
 module.exports = router;
