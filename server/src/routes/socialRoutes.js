5c1d166cb3ab7543718149a9c8213b2e716dd88f
pablo <pablovicenteroura2005@gmail.com>
2025-03-11 18:18:11 +0100

    commit

--- server/src/routes/socialRoutes.js
+++ server/src/routes/socialRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
     analyzeManga,
-} = require('../controllers/mangaontroller.js');
+} = require('../controllers/mangaController.js');
 
 /**
  * @swagger
